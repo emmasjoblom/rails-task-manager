7cf458620fe5c09cf3f767bcd8a7c0e1c0809502
Emma Sjoblom <sjoblom.em@gmail.com>
2018-07-27 16:38:41 +0800

    done excersise 1

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ApplicationRecord
+  # def mark_as_completed
+  #   @task.completed = true
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'tasks', to: 'tasks#index', as: :tasks
+  post 'tasks', to: 'tasks#create'
+  get 'new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id', to: 'tasks#edit', as: :edit_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
